f513108027b32c82fef6480b063b2d70566cccff
Navi0906 <naam0906.be21@chitkara.edu.in>
2025-03-11 15:37:08 +0530

    Naam Simran Singh Add: Displaying Student details

---
+++ Student.java
@@ -0,0 +1,64 @@
+class Student {
+    public int rollNumber;
+    protected String name;
+    private double CGPA;
+
+    // Constructor
+    public Student(int rollNumber, String name, double CGPA) {
+        this.rollNumber = rollNumber;
+        this.name = name;
+        this.CGPA = CGPA;
+    }
+
+    // Public getter for CGPA
+    public double getCGPA() {
+        return CGPA;
+    }
+
+    public void setCGPA(double CGPA) {
+        this.CGPA = CGPA;
+    }
+
+    // Method to display student details
+    public void displayDetails() {
+        System.out.println("Roll Number: " + rollNumber);
+        System.out.println("Name: " + name);
+        System.out.println("CGPA: " + CGPA);
+        System.out.println("------------------------");
+    }
+
+    public static void main(String[] args) {
+        Student student1 = new Student(101, "Dhruv", 8.5);
+        student1.displayDetails();
+
+        student1.setCGPA(9.2);
+        student1.displayDetails();
+
+        PostgraduateStudent pgStudent = new PostgraduateStudent(201, "Madhav", 9.0, "Data Science");
+        pgStudent.displayPostgraduateDetails();
+    }
+
+}
+
+
+class PostgraduateStudent extends Student {
+    private String specialization;
+
+    // Constructor
+    public PostgraduateStudent(int rollNumber, String name, double CGPA, String specialization) {
+        super(rollNumber, name, CGPA);
+        this.specialization = specialization;
+    }
+
+    // Method to display postgraduate student details
+    public void displayPostgraduateDetails() {
+        System.out.println("Postgraduate Student Details:");
+        System.out.println("Roll Number: " + rollNumber);
+        System.out.println("Name: " + name);
+        System.out.println("Specialization: " + specialization);
+        System.out.println("CGPA: " + getCGPA());
+        System.out.println("------------------------");
+    }
+}
+
+
